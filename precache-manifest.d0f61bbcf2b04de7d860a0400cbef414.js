659fb0f0c4a7fb0267f2d8b57e6fb8955841f38c
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-21 13:07:15 +0300

    Updates

---
+++ precache-manifest.d0f61bbcf2b04de7d860a0400cbef414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd9c5453536ac2bf9fef2a2e8450573",
+    "url": "/Calender/index.html"
+  },
+  {
+    "revision": "8b3a04a0012b89bf780a",
+    "url": "/Calender/static/css/main.41b6db9d.chunk.css"
+  },
+  {
+    "revision": "f6cdb29c9954fb9c36f2",
+    "url": "/Calender/static/js/2.08f94fe5.chunk.js"
+  },
+  {
+    "revision": "8b3a04a0012b89bf780a",
+    "url": "/Calender/static/js/main.50554753.chunk.js"
+  },
+  {
+    "revision": "3ecacbc1f3152775ed75",
+    "url": "/Calender/static/js/runtime~main.242bf0c6.js"
+  }
+]);
